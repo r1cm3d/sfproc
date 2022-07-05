464d7dbe1179ee02147e7fc6980f5d1a2a5b794a
r1cm3d <r1cm3d@protonmail.com>
2022-07-05 20:52:00 -0300

    Add AWS PoC
    This patch adds a snipet with a sample request to AWS S3 through AWS
    SDK.
    
    Signed-off-by: r1cm3d <r1cm3d@protonmail.com>

--- src/handler.rs
+++ src/handler.rs
@@ -1,22 +1,34 @@
 use crate::types::{Cli, Writer};
+use aws_sdk_s3::Client;
 use log::info;
+
 
 pub struct Handler {
     writer: Box<dyn Writer>,
+    client: Client,
 }
 
 impl Handler {
-    pub fn new(writer: Box<dyn Writer>) -> Handler {
+    pub fn new(writer: Box<dyn Writer>, client: Client) -> Handler {
         Handler {
             writer,
+            client,
         }
     }
 
-    pub fn handle(&self, args: Cli) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn handle(&self, args: Cli) -> Result<(), Box<dyn std::error::Error>> {
         let endpoint = args.endpoint;
         let pattern = args.pattern;
         let kms_key = args.kms_key;
         let bucket = args.bucket;
+
+        // FIXME: Unstable
+        let resp = self.client.list_objects_v2().bucket(&bucket).send().await?;
+
+        for object in resp.contents().unwrap_or_default() {
+            info!("Object key ({}).", object.key().unwrap_or_default());
+        }
+        // END of unstable code.
 
         info!("Searching files into Bucket ({}) of Endpoint ({}) with Pattern ({}) and KMS \
         Key ({}).", bucket, endpoint, pattern, kms_key);
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,31 @@ use infra::writer::Fs;
 use handler::Handler;
 use types::Cli;
 use clap::Parser;
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::Client;
+use std::error::Error;
 
 mod handler;
 mod infra;
 mod types;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new()
         .with_level(LevelFilter::Info)
         .init()
         .unwrap();
+
+    // FIXME: Unstable
+    let region_provider = RegionProviderChain::default_provider().or_else("sa-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
     let args = Cli::parse();
     let fs_writer = Box::new(Fs {});
-    let handler = Handler::new(fs_writer);
+    let handler = Handler::new(fs_writer, client);
 
-    match handler.handle(args) {
+    match handler.handle(args).await {
         Ok(..) => exit(exitcode::OK),
         Err(e) => {
             error!("Something went wrong. Error: {}", e);
